3f8f25acd31cadb50283d916790b3b9219eb416c
Oualidlaib <93014784+Oualidlaib@users.noreply.github.com>
2023-10-07 17:47:35 +0200

    Menu and Booking Models are added successfully.

--- little-lemon-capstone/littlelemon/restaurant/models.py
+++ little-lemon-capstone/littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from django.core.validators import MaxValueValidator
+class Booking(models.Model):
+  Name = models.CharField(max_length=255)
+  No_of_guests = models.IntegerField()
+  Booking_date = models.DateField()
+  
 
-# Create your models here.
+class Menu(models.Model):
+  Title = models.CharField(max_length=255)
+  Price = models.DecimalField(max_digits=10, decimal_places=2)
+  Inventory = models.IntegerField()
+  
+  
+  
