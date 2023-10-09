8f6df1888ad13b9e88fbebcbd9f6239cfd17bbd8
Oualidlaib <93014784+Oualidlaib@users.noreply.github.com>
2023-10-09 10:48:39 +0200

    Authenticated version

--- little-lemon-capstone/littlelemon/restaurant/views.py
+++ little-lemon-capstone/littlelemon/restaurant/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework import generics, viewsets
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here. 
 class MenuItemsView(generics.ListCreateAPIView):
@@ -12,9 +13,14 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
   queryset = MenuItem.objects.all()
   serializer_class = MenuItemSerializer
   
+
+permission_classes = [IsAuthenticated]
+  
 class BookingViewSet(viewsets.ModelViewSet):
   queryset = Booking.objects.all()
   serializer_class = BookingSerializer
+  
+  
 
 
 
